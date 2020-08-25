35ae1f6e0a5188c24ef1ec6d941532117876f03c
Артем <nikolaev7@inbox.ru>
2020-08-25 10:51:17 +0300

    HTML for dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog+ ' '+s.active}>Dimich</div>
+        <div className={s.dialog}>Andrey</div>
+        <div className={s.dialog}>Svetka</div>
+        <div className={s.dialog}>Petya</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Медленно минуты уплывают вдаль</div>
+        <div className={s.message}>Встречи с ними ты уже не жди</div>
+        <div className={s.message}>И хотя нам прошлого немного жаль</div>
+        <div className={s.message}>Лучшее конечно впереди</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
