09328aba89633ce2f41f2e57457f85bef8a71259
Артем <nikolaev7@inbox.ru>
2020-09-24 12:30:52 +0300

    77. Add form-validation

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,24 +1,16 @@
 import React from "react";
-import styles from "./FormsControls.module.css";
+import s from "./FormsControls.module.css";
 
-const FormControl = ({input, meta, child, ...props}) => {
+const FormControl = FormControl => ({ input, meta, ...props }) => {
     const hasError = meta.touched && meta.error;
     return (
-        <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
-            <div>
-                {props.children}
-            </div>
-            { hasError && <span>{meta.error}</span> }
+        <div className={ s.formControl + " " + (hasError ? s.error : "") }>
+            <FormControl {...input} {...props} />
+            { hasError && <span> { meta.error } </span> }
         </div>
-    )
-}
+    );
+};
 
-export const Textarea = (props) => {
-    const {input, meta, child, ...restProps} = props;
-    return <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
-}
+export const Textarea = FormControl('textarea');
 
-export const Input = (props) => {
-    const {input, meta, child, ...restProps} = props;
-    return <FormControl {...props}><input {...input} {...restProps} /></FormControl>
-}
+export const Input = FormControl('input')
