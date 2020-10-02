c5185b62e32aa73876600dddfa257d0d6380f8f7
Артем <nikolaev7@inbox.ru>
2020-10-02 12:49:03 +0300

    Updates

---
+++ precache-manifest.275c2a99647c1b95367fd99a2db9f751.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578de7afaf740876d38717c9b4dd929b",
+    "url": "/react-way-of-samurai/index.html"
+  },
+  {
+    "revision": "9917907d84a2eeb49169",
+    "url": "/react-way-of-samurai/static/css/3.b7a8f4a5.chunk.css"
+  },
+  {
+    "revision": "6bb3753eb1c68486cce3",
+    "url": "/react-way-of-samurai/static/css/4.cffe4948.chunk.css"
+  },
+  {
+    "revision": "f172ba38591235a1823a",
+    "url": "/react-way-of-samurai/static/css/main.0a213f2a.chunk.css"
+  },
+  {
+    "revision": "0deae78ad261952bcd5f",
+    "url": "/react-way-of-samurai/static/js/2.24f5e3c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-way-of-samurai/static/js/2.24f5e3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9917907d84a2eeb49169",
+    "url": "/react-way-of-samurai/static/js/3.60546c5e.chunk.js"
+  },
+  {
+    "revision": "6bb3753eb1c68486cce3",
+    "url": "/react-way-of-samurai/static/js/4.f4d21ed6.chunk.js"
+  },
+  {
+    "revision": "f172ba38591235a1823a",
+    "url": "/react-way-of-samurai/static/js/main.6b09a5dc.chunk.js"
+  },
+  {
+    "revision": "4efa3146f7b7a8f666e6",
+    "url": "/react-way-of-samurai/static/js/runtime-main.d96b69c3.js"
+  },
+  {
+    "revision": "6a727eda01a5fc2795ceb8d8bac3de12",
+    "url": "/react-way-of-samurai/static/media/Preloader.6a727eda.gif"
+  }
+]);
